9f890dee2beab355b9cd76dc281dd63c6ed0131c
janani-02 <60179684+janani-02@users.noreply.github.com>
2020-02-01 00:45:23 +0530

    multiple inheritance

---
+++ multi_inherit.py
@@ -0,0 +1,40 @@
+
+
+class Bat:
+    species='Baty'
+    def _init_(self,can_fly=True):
+        self.fly=can_fly
+        def say(self,msg):
+            msg='.....'
+            return msg
+        def sonar(self):
+            return'))...(('
+
+if _name_ ==' _main_':
+    b=Bat()
+    print(b.say('hello'))
+    print(b.fly)
+
+
+class Batman(Superhero):
+    def _initr(self,*args,**kwargs):
+        Superhero._init(self,'anonymous',movie=True,superpowers=['Wealthy'],*args,**kwargs)
+        Bat._init_(self,*args,can_fly=false,**kwargs)
+        self.name='Sad Affleck'
+    def sing(self):
+        return'nan nan nan nan batman!'
+if _name_ =='_main_':
+    sup=Batman()
+    print(Batman._mro_)
+    print(sup.get_species())
+    print(sup.sing())
+    sup.say("I agree")
+    print(sup.sonar())
+    sup.age=100
+    print(sup.age)
+    print('can i fly'+str(sup.fly))
+
+
+
+
+    
