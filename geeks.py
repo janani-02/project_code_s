ee0def5aad9f56a197a0bfde3cf03d8ac39ed98d
bhavaniravi <bhava0895@gmail.com>
2020-02-03 19:52:30 +0530

    fix geeks for geeks inheritance

--- geeks.py
+++ geeks.py
@@ -1,19 +1,20 @@
 class Base1(object):
-    def _init_(self):
+    def __init__(self):
         self.str1="Geek1"
         print ("Base1")
 
 
 class Base2(object):
-    def _init_(self):
+    def __init__(self):
         self.str2="Geek2"
         print ("Base2")
 
 class Derived(Base1,Base2):
-    def _init_(self):
-        Base1._init_(self)
-        Base2._init_(self)
+    def __init__(self):
+        Base1.__init__(self)
+        Base2.__init__(self)
         print("Derived")
+    
     def printStrs(self):
         print(self.str1,self.str2)
 ob = Derived()
